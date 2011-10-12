493cd24da98212a5e7103e88d91e990cea384f65
Jaron Kennel <jaronkk@gmail.com>
2011-10-12 16:51:12 -0400

    reenabling ddw

--- app/models/ddw_database.rb
+++ app/models/ddw_database.rb
@@ -1,5 +1,5 @@
-class DDWDatabase #< ActiveRecord::Base
-  #establish_connection "ddw_#{Rails.env}"
+class DDWDatabase < ActiveRecord::Base
+  establish_connection "ddw_#{Rails.env}"
   @abstract_class = true
   
   # This hook is called when DDWDatabase is subclassed.  We use it to set
@@ -8,28 +8,9 @@ class DDWDatabase #< ActiveRecord::Base
     super
     table_name = c.name.tableize.gsub(/^ddw_/,'')
     id_field = table_name.singularize + "_id"
-    #c.set_table_name(table_name)
-    #c.set_primary_key(id_field)
+    c.set_table_name(table_name)
+    c.set_primary_key(id_field)
   end
-  
-  
-  def self.set_table_name(value)
-    
-  end
-  
-  def self.set_primary_key(value)
-    
-  end
-  
-  def self.has_many(*args)
-  end
-  
-  def self.has_and_belongs_to_many(*args)
-  end
-  
-  def self.belongs_to(*args)
-  end
-  
   
   # This application should not be writing to the DDW database
   def readonly?
--- app/models/ddw_resource.rb
+++ app/models/ddw_resource.rb
@@ -1,9 +1,8 @@
 class DDWResource < DDWDatabase
   has_many :terms_resources, :class_name => "DDWTermsResource", :foreign_key => "resource_id"
   has_many :terms, :through => :terms_resources
-  #has_and_belongs_to_many :subject_terms, :class_name => "DDWTerm", :join_table => "terms_resources", :foreign_key => "resource_id", :association_foreign_key => "term_id", :conditions => {"terms.facet_id" => DDWFacet.subject.id}
+  has_and_belongs_to_many :subject_terms, :class_name => "DDWTerm", :join_table => "terms_resources", :foreign_key => "resource_id", :association_foreign_key => "term_id", :conditions => {"terms.facet_id" => DDWFacet.subject.id}
   has_many :locations, :class_name => "DDWResourceLocation", :foreign_key => "resource_id"
-  
   
   def subject_term
     self.subject_terms.first
