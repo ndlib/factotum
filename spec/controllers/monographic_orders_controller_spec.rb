10cb4096ee8c3aa218a2578bfd4de5cd3f0cde57
Jaron Kennel <jaronkk@gmail.com>
2013-01-30 10:56:04 -0500

    Improved login testing

--- spec/controllers/monographic_orders_controller_spec.rb
+++ spec/controllers/monographic_orders_controller_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
 describe MonographicOrdersController do
-  it "should have a functioning new page" do
-    get 'new'
-    response.should be_success
-  end
-  
-  it "should allow new orders to be made" do
-    record = FactoryGirl.build(:monographic_order)
-    post 'create', :order => record.attributes
-    response.should be_redirect
-    new_record = assigns(:monographic_order)
-    response.should redirect_to(success_monographic_orders_path())
+  describe "user" do
+    login_user
+
+    it "should have a functioning new page" do
+      get 'new'
+      response.should be_success
+    end
+    
+    it "should allow new orders to be made" do
+      record = FactoryGirl.build(:monographic_order)
+      post 'create', :order => record.attributes
+      response.should be_redirect
+      monographic_order = assigns(:monographic_order)
+      monographic_order.should be_a_kind_of MonographicOrder
+      monographic_order.should be_valid
+      response.should redirect_to(success_monographic_orders_path())
+    end
   end
 end
