fa6f9ef1d2fad12684f3174d23a16736732f0275
Jaron Kennel <jaronkk@gmail.com>
2013-02-07 16:02:05 -0500

    Testing pagination on monographic order controller,  cleaned up spec, converted eq() to ==

--- spec/controllers/monographic_orders_controller_spec.rb
+++ spec/controllers/monographic_orders_controller_spec.rb
@@ -13,8 +13,23 @@ describe MonographicOrdersController do
       it "lists orders created by this user" do
         orders = FactoryGirl.create_list(:monographic_order, 2, creator: subject.current_user)
         get :index
-        monographic_orders = assigns(:monographic_orders)
-        monographic_orders.count.should eq(2)
+        assigns(:monographic_orders).count.should == 2
+      end
+
+      describe "pagination" do
+        before do
+          @orders = FactoryGirl.create_list(:monographic_order, 40, creator: subject.current_user)
+        end
+
+        it "limits to 25 results" do
+          get :index
+          assigns(:monographic_orders).count.should == 25
+        end
+
+        it "displays results on the second page" do
+          get :index, page: 2
+          assigns(:monographic_orders).count.should == 15
+        end
       end
     end
 
@@ -22,13 +37,14 @@ describe MonographicOrdersController do
       it "prepopulates with data from last order" do 
         previous = FactoryGirl.create(:monographic_order, creator: subject.current_user)
         get :new
+        response.should be_success
         monographic_order = assigns(:monographic_order)
         monographic_order.should be_a_kind_of MonographicOrder
-        monographic_order.selector.should eq(previous.selector)
-        monographic_order.fund.should eq(previous.fund)
-        monographic_order.fund_other.should eq(previous.fund_other)
-        monographic_order.cataloging_location.should eq(previous.cataloging_location)
-        monographic_order.cataloging_location_other.should eq(previous.cataloging_location_other)
+        monographic_order.selector.should == previous.selector
+        monographic_order.fund.should == previous.fund
+        monographic_order.fund_other.should == previous.fund_other
+        monographic_order.cataloging_location.should == previous.cataloging_location
+        monographic_order.cataloging_location_other.should == previous.cataloging_location_other
       end
     end
     
@@ -40,7 +56,7 @@ describe MonographicOrdersController do
         monographic_order = assigns(:monographic_order)
         monographic_order.should be_a_kind_of MonographicOrder
         monographic_order.should be_valid
-        monographic_order.creator.should eq(subject.current_user)
+        monographic_order.creator.should == subject.current_user
         response.should redirect_to(monographic_order_path(monographic_order))
       end
     end
@@ -58,8 +74,7 @@ describe MonographicOrdersController do
       it "lists orders for this selector" do
         orders = FactoryGirl.create_list(:monographic_order, 2, selector: subject.current_user.selector)
         get :index
-        monographic_orders = assigns(:monographic_orders)
-        monographic_orders.count.should eq(2)
+        assigns(:monographic_orders).count.should == 2
       end
     end
 
@@ -67,13 +82,14 @@ describe MonographicOrdersController do
       it "prepopulates with data from last order and assigns selector as current selector" do 
         previous = FactoryGirl.create(:monographic_order, creator: subject.current_user)
         get :new
+        response.should be_success
         monographic_order = assigns(:monographic_order)
         monographic_order.should be_a_kind_of MonographicOrder
-        monographic_order.selector.should eq(subject.current_user.selector)
-        monographic_order.fund.should eq(previous.fund)
-        monographic_order.fund_other.should eq(previous.fund_other)
-        monographic_order.cataloging_location.should eq(previous.cataloging_location)
-        monographic_order.cataloging_location_other.should eq(previous.cataloging_location_other)
+        monographic_order.selector.should == subject.current_user.selector
+        monographic_order.fund.should == previous.fund
+        monographic_order.fund_other.should == previous.fund_other
+        monographic_order.cataloging_location.should == previous.cataloging_location
+        monographic_order.cataloging_location_other.should == previous.cataloging_location_other
       end
     end
     
@@ -85,8 +101,8 @@ describe MonographicOrdersController do
         monographic_order = assigns(:monographic_order)
         monographic_order.should be_a_kind_of MonographicOrder
         monographic_order.should be_valid
-        monographic_order.creator.should eq(subject.current_user)
-        monographic_order.selector.should eq(subject.current_user.selector)
+        monographic_order.creator.should == subject.current_user
+        monographic_order.selector.should == subject.current_user.selector
         response.should redirect_to(monographic_order_path(monographic_order))
       end
     end
@@ -104,8 +120,7 @@ describe MonographicOrdersController do
       it "lists all orders" do
         orders = FactoryGirl.create_list(:monographic_order, 2)
         get :index
-        monographic_orders = assigns(:monographic_orders)
-        monographic_orders.count.should eq(2)
+        assigns(:monographic_orders).count.should == 2
       end
     end
   end
