94ff97ac942bef168c2f2b2cdec3feef0471d614
Jaron Kennel <jaronkk@gmail.com>
2013-02-07 14:53:32 -0500

    Disabling SSI file includes when layout is rendered, added named methods for removing stubs

--- spec/support/global_stubs.rb
+++ spec/support/global_stubs.rb
@@ -1,5 +1,34 @@
 module GlobalStubs
   def add_global_stubs
+    add_user_ldap_stub
+    add_asset_ssi_stub
+  end
+
+  def add_user_ldap_stub
     User.any_instance.stub(:store_ldap_attributes).and_return(true)
+  end
+
+  def remove_user_ldap_stub
+    User.any_instance.unstub(:store_ldap_attributes)
+  end
+
+  def add_asset_ssi_stub
+    AssetsHelper.class_eval do
+      unless method_defined? :read_ssi_file_original
+        alias_method :read_ssi_file_original, :read_ssi_file
+        def read_ssi_file(*args)
+          ""
+        end
+      end
+    end
+  end
+
+  def remove_asset_ssi_stub
+    AssetsHelper.class_eval do
+      if method_defined? :read_ssi_file_original
+        alias_method :read_ssi_file, :read_ssi_file_original
+        remove_method :read_ssi_file_original
+      end
+    end
   end
 end
