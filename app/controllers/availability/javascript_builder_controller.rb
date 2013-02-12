e286aad7d3ba71fc9e1eb253917cdf0bd0809b61
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-02-12 16:16:22 -0500

    fix silly bug where there are no 2 columns for the builder

--- app/controllers/availability/javascript_builder_controller.rb
+++ app/controllers/availability/javascript_builder_controller.rb
@@ -3,7 +3,7 @@ class Availability::JavascriptBuilderController < ApplicationController
 
   helper_method :current_url
 
-  def index
+  def index  	
     @selected_service_points = Availability::ServicePoint.where("code IN(?)", params[:selected_service_points]) || []
   end
 
