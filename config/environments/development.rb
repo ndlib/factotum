b68ebcfbce7ce0a642bf34f58b068657919ba686
Robert Fox <rfox2@nd.edu>
2013-02-28 16:23:40 -0500

    Adding Action::Mailer test setting for dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,10 @@ Factotum::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => "factotum.dev" }
   
+  # Action mailer settings
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => "localhost" }
+  
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
