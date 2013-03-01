e7117b8066cc7d83b8008d2812098d5ac95ed0f8
Robert Fox <rfox2@nd.edu>
2013-03-01 10:26:15 -0500

    Changing preprod email for map requests so that it uses the circ account

--- config/environments/pre_production.rb
+++ config/environments/pre_production.rb
@@ -64,5 +64,5 @@ Factotum::Application.configure do
   # Custom configuration
   config.hours_api_uri = 'http://apipprd.library.nd.edu/1.0/resources/availability/hours'
   config.cas_base = 'https://login-test.cc.nd.edu/cas'
-  config.maps_mail_target = 'rfox2@nd.edu'
+  config.maps_mail_target = 'circ@nd.edu'
 end
