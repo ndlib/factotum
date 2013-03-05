1f50e634f1a9c8a77fb98899806d6702bf0fff9d
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-03-05 11:14:08 -0500

    fixed the seed parse error :D

--- db/migrate/20130305161119_preprod_seeds.rb
+++ db/migrate/20130305161119_preprod_seeds.rb
@@ -1,17 +1,17 @@
 class PreprodSeeds < ActiveRecord::Migration
   def up
     [ [ "Hesburgh", 'hesburgh'] ,
-  [ "Engineering", 'engineering'],
-  [ "Architecture", 'architecture'], 
-  [ "Life Sciences", 'life_sciences'], 
-  [ "Chemistry and Physics", 'chemistry_and_physics'], 
-  [ "Kellog/Kroc Information Center", 'kellog_kroc'], 
-  [ "Business Information Center", 'bic'], 
-  [ "Mathematics", 'math'], 
-  [ "Radiation Laboratory", 'radiation'], 
-  [ "None", 'none']].each do | lib |
-  Building.create!(:name => lib[0], :search_code => lib[1])
-
+      [ "Engineering", 'engineering'],
+      [ "Architecture", 'architecture'], 
+      [ "Life Sciences", 'life_sciences'], 
+      [ "Chemistry and Physics", 'chemistry_and_physics'], 
+      [ "Kellog/Kroc Information Center", 'kellog_kroc'], 
+      [ "Business Information Center", 'bic'], 
+      [ "Mathematics", 'math'], 
+      [ "Radiation Laboratory", 'radiation'], 
+      [ "None", 'none']].each do | lib |
+        Building.create!(:name => lib[0], :search_code => lib[1])
+      end
   end
 
   def down
