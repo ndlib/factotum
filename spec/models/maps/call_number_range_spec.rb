e9b0ddbe5ff2fe0b11c7ed3e38fb96377d199ad1
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-03-05 11:00:32 -0500

    simplify spec.

--- spec/models/maps/call_number_range_spec.rb
+++ spec/models/maps/call_number_range_spec.rb
@@ -21,52 +21,34 @@ describe Maps::CallNumberRange do
     let(:valid_params) { { collection_code: 'collection', sublibrary_code: 'sublibrary', begin_call_number: '1111', end_call_number: '2222', floor_map_id: floor_map.id } }
 
     it "saves with valid parameters" do
-      cnr = Maps::CallNumberRange.new(valid_params)
+      cnr = call_number_range.class.new(valid_params)
       cnr.valid?.should be_true
     end
 
 
-    it "requires collection_code" do 
-      params = valid_params 
-      params.delete(:collection_code)
 
-      cnr = Maps::CallNumberRange.new(valid_params)
-      cnr.valid?.should be_false      
+    it "requires collection_code" do 
+      call_number_range.class.new.should have(1).error_on(:collection_code)
     end
 
 
     it "requires sublibrary_code" do 
-      params = valid_params
-      params.delete(:sublibrary_code)
-
-      cnr = Maps::CallNumberRange.new(valid_params)
-      cnr.valid?.should be_false      
+      call_number_range.class.new.should have(1).error_on(:sublibrary_code)     
     end
 
 
     it "requires a start_range" do 
-      params = valid_params
-      params.delete(:begin_call_number)
-
-      cnr = Maps::CallNumberRange.new(valid_params)
-      cnr.valid?.should be_false      
+      call_number_range.class.new.should have(1).error_on(:begin_call_number)         
     end
 
-    it "requires an end_range" do 
-      params = valid_params
-      params.delete(:end_call_number)
 
-      cnr = Maps::CallNumberRange.new(valid_params)
-      cnr.valid?.should be_false      
+    it "requires an end_range" do 
+      call_number_range.class.new.should have(1).error_on(:end_call_number)          
     end
 
 
     it "requires a floor_map" do 
-      params = valid_params
-      params.delete(:floor_map_id)
-
-      cnr = Maps::CallNumberRange.new(valid_params)
-      cnr.valid?.should be_false      
+      call_number_range.class.new.should have(1).error_on(:floor_map)      
     end
   end
 end
