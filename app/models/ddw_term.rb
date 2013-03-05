6dc05db174a4a9415276c75679411d98436c2a32
Jaron Kennel <jaronkk@gmail.com>
2013-03-05 11:53:56 -0500

    Fixed a bug since I converted subject_terms to a method rather than an association

--- app/models/ddw_term.rb
+++ app/models/ddw_term.rb
@@ -19,7 +19,7 @@ class DDWTerm < DDWDatabase
   
   belongs_to :facet, :class_name => "DDWFacet", :foreign_key => "facet_id"
   has_many :terms_resources, :class_name => "DDWTermsResource", :foreign_key => "term_id"
-  has_many :resources, :through => :terms_resources, :include => [:subject_terms]
+  has_many :resources, :through => :terms_resources
   has_and_belongs_to_many :librarians, :class_name => "DDWLibrarian", :join_table => "terms_librarians", :foreign_key => "term_id", :association_foreign_key => "librarian_id"
   
   def self.default_order
