efb8062f387eee231eb60eb6ef1a9db660e6e161
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-03-08 11:17:36 -0500

    added tests for the api search.

--- spec/models/hours_api_spec.rb
+++ spec/models/hours_api_spec.rb
@@ -36,4 +36,33 @@ describe HoursApi do
     end
 
   end
+
+
+  describe "#api_services_from_request" do
+
+    it "returns all the service points if no codes are specified" do
+      service_points
+
+      params = {}
+      hours_api.api_services_from_request(params).size == service_points.size
+    end
+
+
+    it "returns just the service point specified in the codes" do
+      params = { service_codes: service_points.first.code }
+
+      hours_api.api_services_from_request(params).size == 1
+      hours_api.api_services_from_request(params).first.id == service_points.first.id
+    end
+
+
+    it "returns each of the codes speificed in the csv list " do
+      params = { service_codes: "#{service_points.first.code}, #{service_points.last.code}" }
+
+      res = hours_api.api_services_from_request(params)
+      res.size == 2
+      res.first.id == service_points.first.id
+      res.last.id  == service_points.first.id
+    end
+  end
 end
