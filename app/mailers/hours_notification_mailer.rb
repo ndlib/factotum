f5fb656c5cb561b99952adac92def511e08ed90f
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-03-15 13:21:52 -0400

    small changes to the notification.

--- app/mailers/hours_notification_mailer.rb
+++ app/mailers/hours_notification_mailer.rb
@@ -2,7 +2,7 @@ class HoursNotificationMailer < ActionMailer::Base
 
   def send_all_notifictions
     services = Availability::ServicePoint.all
-    services.each do | sp | 
+    services.each do | sp |
       if sp.notification_emails.present?
         self.class.send_check_hours_notification(sp).deliver
       end
@@ -10,8 +10,8 @@ class HoursNotificationMailer < ActionMailer::Base
   end
 
 
-  def send_check_hours_notification(service)     
-    @service_point = service
+  def send_check_hours_notification(service)
+    @service_point = HoursApi.new(ApplicationController.new).service_point(service.id)
     mail :from => 'update_hours_notifier@nd.edu', :to => service.notification_emails, :subject => "Verify Hours are still correct for #{service.name}"
   end
 
