286adf58dc49881700ebaf0f38f17beacdc47ad2
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-03-25 11:17:23 -0400

    only needed because i am a terrible speller.

--- config/environments/pre_production.rb
+++ config/environments/pre_production.rb
@@ -63,7 +63,7 @@ Factotum::Application.configure do
 
   # Custom configuration
   config.base_url = "https://factotumpprd.library.nd.edu"
-  config.hours_api_uri = 'https://apipprd.library.nd.edu/1.0/locaitons/hours'
+  config.hours_api_uri = 'https://apipprd.library.nd.edu/1.0/locations/hours'
   config.cas_base = 'https://login-test.cc.nd.edu/cas'
   config.maps_mail_target = 'circ@nd.edu'
 end
