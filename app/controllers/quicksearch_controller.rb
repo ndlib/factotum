3d714c5fd3bf02a1796119094626c18debd3bfad
Jaron Kennel <jaronkk@gmail.com>
2013-04-12 11:52:08 -0400

    Updated xerxes_domain to be a configuration option rather than a constant

--- app/controllers/quicksearch_controller.rb
+++ app/controllers/quicksearch_controller.rb
@@ -12,13 +12,13 @@ class QuicksearchController < ApplicationController
     ExceptionNotifier::Notifier.exception_notification(request.env, exception).deliver
     redirect_to xerxes_url(params[:target])
   end
-  
+
   private
     def xerxes_url(target)
       target = target.to_s
       if (target =~ /^\/quicksearch/).nil?
         target = "/quicksearch/"
-      end      
-      "http://#{XERXES_DOMAIN}" + target
+      end
+      "http://#{Rails.configuration.xerxes_domain}" + target
     end
 end
--- config/environments/pre_production.rb
+++ config/environments/pre_production.rb
@@ -59,7 +59,7 @@ Factotum::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  XERXES_DOMAIN = "xerxespprd.library.nd.edu"
+  config.xerxes_domain = "xerxespprd.library.nd.edu"
 
   # Custom configuration
   config.base_url = "https://factotumpprd.library.nd.edu"
