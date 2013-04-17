d33992aa4f12d8c0d85bd4f6803c49aedae5de52
Jaron Kennel <jaronkk@gmail.com>
2013-04-17 11:08:43 -0400

    Added view spec to verify architecture branch styles render when assets are precompiled

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -18,5 +18,35 @@ describe "layouts/application.html.erb" do
     it "renders" do
       render
     end
+
+    describe 'main' do
+      before do
+        view.stub(:active_branch_code).and_return('main')
+      end
+
+      it "renders" do
+        render
+      end
+    end
+
+    describe 'architecture' do
+      before do
+        view.stub(:active_branch_code).and_return('architecture_library')
+      end
+
+      it "renders" do
+        render
+      end
+    end
+
+    describe 'invalid branch' do
+      before do
+        view.stub(:active_branch_code).and_return('fake_library')
+      end
+
+      it "raises ActionView::Template::Error" do
+        expect { render }.to raise_error ActionView::Template::Error
+      end
+    end
   end
 end
