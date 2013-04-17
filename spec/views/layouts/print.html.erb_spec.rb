cd3bb7b02a5ee2cd1ea8e678d37879b5918fc586
Jaron Kennel <jaronkk@gmail.com>
2013-04-17 10:59:14 -0400

    Added precompile test for print layout

--- spec/views/layouts/print.html.erb_spec.rb
+++ spec/views/layouts/print.html.erb_spec.rb
@@ -5,4 +5,18 @@ describe "layouts/print.html.erb" do
   it "should render" do
     render
   end
+
+  describe 'precompiled assets' do
+    before(:all) do
+      enable_asset_precompiling
+    end
+
+    after(:all) do
+      disable_asset_precompiling
+    end
+
+    it "renders" do
+      render
+    end
+  end
 end
