652a31a6c57f9ba226ecb53c0d1c479790d0e09c
Robin Schaaf <robinmschaaf@gmail.com>
2013-09-13 14:36:43 -0400

    fixing db migrate issue

--- db/migrate/20130901222157_create_formats_locations_associative.rb
+++ db/migrate/20130901222157_create_formats_locations_associative.rb
@@ -1,5 +1,6 @@
 class CreateFormatsLocationsAssociative < ActiveRecord::Migration
   def change
+        drop_table :cataloging_locations_formats
 
 	create_table :cataloging_locations_formats, :id => false do |t|
   		t.references :cataloging_locations, :null => false
