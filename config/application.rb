af1ee5e3d835c732d5056c479d06c4401400fee5
Robert Fox <robertjonfox@gmail.com>
2013-09-23 16:50:50 -0400

    Add additional load paths

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,9 @@ module Factotum
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths += Dir[Rails.root.join('lib', '{**}')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'controllers', '{**}')]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
