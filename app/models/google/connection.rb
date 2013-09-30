ea67417a2700b46063ba96a5bb926c6a4d4d93f8
Robert Fox <robertjonfox@gmail.com>
2013-09-29 21:10:03 -0400

    Initial models for Google integration.

---
+++ app/models/google/connection.rb
@@ -0,0 +1,18 @@
+class Google::Connection
+
+  attr_reader :session
+
+  def initialize
+    begin
+      @session = GoogleDrive.login(ENV["google_username"], ENV["google_password"])
+    rescue GoogleDrive::AuthenticationError => e
+      Rails.logger.warn(e.message)
+      raise e
+    end
+  end
+
+  def auth_token
+    @session.auth_token
+  end
+
+end
