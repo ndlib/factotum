c173050de60178cc916f473816b99098dfe3dcec
Robin Schaaf <robinmschaaf@gmail.com>
2013-11-18 15:57:07 -0500

    organization page and sidebar nav

--- app/helpers/directory_helper.rb
+++ app/helpers/directory_helper.rb
@@ -1,5 +1,15 @@
 module DirectoryHelper
 
+  def directory_menu
+    content_for(:left_column, render(:partial => "shared/directory_menu"))
+  end
+
+
+  def css_current_page_class(path)
+	"disabled" if current_page?(path)
+  end
+
+
 
   def permission
   	if current_user
