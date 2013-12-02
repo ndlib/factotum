da594a16e31aa5841a81fc23eb1ad7ed76077d17
Robert Fox <robertjonfox@gmail.com>
2013-12-02 09:43:28 -0500

    Minor updates to some files.

--- app/controllers/directory/organization_controller.rb
+++ app/controllers/directory/organization_controller.rb
@@ -1,21 +1,11 @@
 class Directory::OrganizationController < ApplicationController
-require 'pry'
 
 
   def index
     # should be only one - administration
     top_level = DirectoryDepartment.top_level
     @all_departments = top_level.self_and_descendants_by_level.flatten
-#binding.pry
   end
-
-
-
-
-
-
-
-
 
 
 end
--- app/models/directory/active_record/directory_department.rb
+++ app/models/directory/active_record/directory_department.rb
@@ -9,9 +9,6 @@ class DirectoryDepartment < DirectoryOrganizationalUnit
   end
 
 
-
-
-
   def descendants_by_level(i=1)
     children_departments.sorted.map do |ch|
       [:directory_department => ch, :level => i] << [ch.descendants_by_level(i+1)]
@@ -22,7 +19,6 @@ class DirectoryDepartment < DirectoryOrganizationalUnit
   def self_and_descendants_by_level
     [:directory_department => self, :level => 1] << descendants_by_level(2)
   end
-
 
 
 end
