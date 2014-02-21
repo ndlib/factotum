80ce5abb7f976fa83f6c3717c8d25601e1ccaa9c
Jaron Kennel <jaronkk@gmail.com>
2014-02-21 08:59:53 -0500

    Updated schedule.rb for the vm

--- config/schedule.rb
+++ config/schedule.rb
@@ -17,30 +17,8 @@
 #   runner "AnotherModel.prune_old_records"
 # end
 
-if environment == 'pre_production'
-  set :bundler, "/shared/ruby_pprd/ruby/1.9.3/bin/bundle"
-elsif environment == 'production'
-  set :bundler, "/shared/ruby_prod/ruby/1.9.3/bin/bundle"
-else
-  set :bundler, "bundle"
-end
-
-
-if environment == 'pre_production' || environment == 'production'
-  set :rails_exec, 'vendor/bundle/bin/rails'
-else
-  set :rails_exec, 'rails'
-end
-
-if environment == 'pre_production' || environment == 'production'
-  set :rake_exec, 'vendor/bundle/bin/rake'
-else
-  set :rake_exec, 'rake'
-end
-
-
-job_type :runner, "cd :path && :bundler exec :rails_exec runner -e :environment ':task' :output"
-job_type :rake,   "cd :path && :environment_variable=:environment :bundler exec :rake_exec :task --silent :output"
+job_type :runner, "cd :path && bundle exec rails runner -e :environment ':task' :output"
+job_type :rake,   "cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
 
 # Learn more: http://github.com/javan/whenever
 
