2953b6f0d315abf3be56c0f9f83afacdc784648d
Dan Wolfe <dwolfe2@nd.edu>
2014-06-16 10:54:39 -0400

    Add update_directory.sql file to update directory data to more recent dump taken on June 13 2014.

---
+++ db/update_directory.sql
@@ -0,0 +1,282 @@
+
+UPDATE directory_employees SET about_text="
+Akilah is the primary customer service representative for guests and fellow Library employees visiting or calling the Library Administration Office. She also provides day-to-day support to the Associate University Librarians and executive administrators.
+
+Administrative Support
+----------------------
+ - Schedule meetings and manage calendars for executive administrators and Program Directors.
+ - Materials preparation for executive administrators.
+ - Order office supplies through Office Depot for all library personnel.
+ - Serve as back up for Ultratime and TravelND systems.
+ - Update faculty and staff member information on the Libraries’ directories and listservs.
+ - Schedule classrooms and assign librarians for library instruction sessions.
+ - Supervise student employees for the Library Administration.
+
+
+Customer Service
+----------------
+ - Greet visitors, answer questions, and manage phone calls to the Administrative offices.
+ - Provide assistance to faculty and staff preparing for meetings, projects, and library instruction sessions.
+
+
+Special Project Support
+-----------------------
+ - Assist with PEEPs and other Libraries recognition initiatives.
+ - Event planning for employee functions and student related activities.
+ - Make reservations and place catering orders.
+ - Special purchases for outreach activities, such as fliers and handouts from FedEx, to promote the Libraries.
+ - Design and administer web page for Administrative and Central Resources Program.
+" WHERE netid = "adixon4";
+
+
+
+
+UPDATE directory_employees SET photo = "http://library.nd.edu/about/departments/adminoff/images/AkilahDixon.jpg" WHERE netid = "adixon4";
+
+
+
+
+
+
+
+
+
+
+UPDATE directory_employees SET about_text="
+As the Organizational Development Librarian, Marcy assesses and enhances the effectiveness of the Libraries’ organizational structure and operations by working closely with cabinet members, supervisors, faculty colleagues, library teams, and campus partners.
+
+Human Resources
+---------------
+
+ - Integrate the University and Library-wide goals, values, and perspectives into the Libraries’ internal functions, such as position design, the recruitment and orientation processes, goal statements, assessment and recognition, team composition, program planning, and communication.
+
+ - Work with CAP, ELF, and individual supervising librarians to ensure that CAP and faculty manual guidelines are regularly reviewed, maintained, and communicated.
+
+Organizational Development
+--------------------------
+
+ - Integrate goals, values, and culture statements into position design, recruitment process, performance management, recognition and internal communication efforts.
+
+ - Oversee the annual review process for both staff and faculty.
+
+ - Administer the local training and development budget.
+
+ - Provide informal coaching and structured training to strengthen individual, management, and team effectiveness.
+
+ - Oversee OD-related library teams such as the May/June Institute Team, the Performance Recognition Awards Team, the Faculty and Staff Training and Development Team, the Professional Development Committee, the Orientation Team, and the ND Voice Team.
+
+
+Recruitment
+-----------
+ - Assist in developing recruitment strategies for faculty positions and designing position descriptions for faculty and for staff in the Library Family of the HR job architecture.
+" WHERE netid = "msimons";
+
+
+UPDATE directory_employees SET photo = "http://library.nd.edu/about/departments/adminoff/images/MarcySimons.2.jpg" WHERE netid = "msimons";
+
+
+UPDATE directory_employees SET about_text="
+As the Director of the Administrative and Central Resources Program, Matt oversees the financial operating framework for the Hesburgh Libraries and is also responsible for human resources administration and building maintenance.
+
+Financial Oversight
+-------------------
+
+ - Oversee annual budget for Hesburgh Libraries.
+ - Steward endowments, library priorities, and donor designations.
+ - Develop short-term and long-term budget priorities and strategies.
+ - Coordinate financial operations with Office of the Provost.
+ - Manage year-to-date financial performance and year-end performance versus budget.
+ - Develop and recommend policies specific to the Hesburgh Libraries.
+ - Serve as primary liaison with University financial offices.
+
+
+## Personnel and Staffing ##
+ - Ensure compliance with University policies and procedures governing all personnel areas: recruiting, hiring, appointments, promotion, attendance, payroll, separation, and FMLA.
+ - Manage communication of personnel activities and issues between the Hesburgh Libraries, Human Resources, and the Provost’s Office.
+ - Develop and recommend policies specific to the Hesburgh Libraries.
+
+## Manage Program Area ##
+ - Supervise employees in the Administrative and Central Resources Program.
+ - Participate on senior leadership teams, representing interests of the Program.
+ - Communicate Library initiatives to Program staff, ensuring their engagement and understanding.
+ - Assist in strategic planning for the Library, and lead goal planning for the Program.
+" WHERE netid = "msimpson";
+
+
+UPDATE directory_employees SET photo = "http://library.nd.edu/about/departments/adminoff/images/MatthewSimpson.jpg" WHERE netid = "msimpson";
+
+
+
+
+UPDATE directory_employees SET about_text="
+Salem is primarily responsible for the procurement activities of the Library, reviewing expense reimbursement activity in the TravelND system and non-computer purchases in BuyND. He also assists the Director and the Senior Accounting Specialist with accounting activities.
+
+TravelND
+--------
+ - Audit all expense reports submitted by faculty and staff.
+ - Assist some faculty and staff with entering their expenses into TravelND.
+ - Answer questions about expense reimbursement, including issues with expense reports submission.
+ - Help assemble and submit expense reports for the senior leaders in the Library Administration office.
+
+Procurement /BuyND
+------------------
+ - Purchase all non-computer equipment and furniture for the libraries.
+ - Purchase many miscellaneous items for faculty and staff.
+ - Research and locate items for procurement that employees need but cannot easily find on their own.
+ - Coordinate purchases with many different departments on campus, working closely with the Controller’s Group, especially the Procurement Office.
+ - Ensure Library purchasing activities follow University guidelines and procedures, as communicated by the Controller’s Procurement Office.
+
+Accounting
+----------
+ - Update the Information Technology Budget Report on a quarterly basis.
+ - Act as Accounts Payable for most of the Library’s expenses (that are not related to materials acquisitions, resources deliver, or Interlibrary Loan services.
+ - Answer general questions related to budgets, expenses, and account code (FOAPAL) use.
+" WHERE netid = "sdavis1";
+
+
+UPDATE directory_employees SET photo = "http://library.nd.edu/about/departments/adminoff/images/SalemDavis.jpg" WHERE netid = "sdavis1";
+
+
+
+UPDATE directory_employees SET about_text="
+As Building Services Manager, Ross is responsible for general maintenance and space management of the Hesburgh Library building, as well representing Hesburgh Library on all facilities and equipment projects, promoting safety and security for the building and managing the security monitors.
+
+Building Manager
+----------------
+ - Coordinate renovations, alterations, office relocations, and new construction.
+ - Monitor IAQ throughout the library and contact Utilities as necessary.
+ - Maintain furniture inventory.
+ - Coordinate with Building Services concerning cleanliness issues.
+ - Supervise Building Services Assistant
+
+Safety and Security Officer
+---------------------------
+ - Supervise Library Safety and Security Monitors.
+ - Maintain Incident Report Log.
+ - Maintain key inventory and disseminate keys as necessary.
+ - Respond to safety and security concerns throughout the building.
+ - Chair the Library Safety and Security Task Force.
+ - Coordinate with Notre Dame Security Police.
+
+Maintenance Coordinator
+-----------------------
+ - Report through the AIM software system all issues involving outside library support.
+ - Follow up on all maintenance repairs and replacements.
+ - Coordinate the follow-through and completion of all work orders from University departments.
+ - Coordinate with the Maintenance Project Coordinator on alterations to the Hesburgh Library and associated branches.
+" WHERE netid = "rfergers";
+
+
+UPDATE directory_employees SET photo = "http://library.nd.edu/about/departments/adminoff/images/RossFergerson.jpg" WHERE netid = "rfergers";
+
+
+
+
+
+UPDATE directory_employees SET about_text="
+In close coordination with the Director, Cynthia is responsible for the support of all financial functions including general accounting, budgeting, financial analysis, and statistical reporting.
+
+Budget
+------
+
+ - Prepares Library’s materials acquisition budget (approximately $11 million).
+ - Assist management with the computation and analysis for the personnel and operations budget (approximately $15 million).
+ - Compile the Library’s annual and ad hoc statistical and salary surveys, including reports to the Association of Research Libraries, the NationalCenter for Education Statistics, Academic Libraries Survey, various salary surveys, and other statistical reports.
+
+Purchasing and Travel Reimbursement
+-----------------------------------
+ - Library resource for the TravelND system as a Super User.
+ - Review and approve travel and expense reimbursement requests in Concur.
+ - Review and approve Library purchases by check request and in BuyND.
+" WHERE netid = "charris9";
+
+
+UPDATE directory_employees SET photo = "http://library.nd.edu/about/departments/adminoff/images/CynthiaHarris.jpg" WHERE netid = "charris9";
+
+
+
+
+
+UPDATE directory_employees SET about_text="
+Reporting to the Director, Michelle administers the Libraries’ human resources and payroll functions, including recruiting and hiring activities, preparing and managing employee records, and guiding managers and supervisors on University personnel policies and procedures.
+
+Hiring
+------
+
+ - Faculty: Support Faculty Search and CAP committees.
+ - Faculty: Coordinate the reappointment and promotion process.
+ - Staff: – Work with supervisors to post and recruit for staff positions.
+ - Staff: Fill temporary and summer employment positions.
+ - Students: Point of contact for students interested in library employment; refers students to supervisors with open positions in their departments.
+
+Payroll
+-------
+
+ - Coordinate Ultra Time hourly payroll for the Libraries.
+ - Work with Payroll Services to resolve questions and concerns regarding hourly, semi-monthly, and bi-weekly payrolls.
+ - Ensure accurate pay for employees who are on FMLA leave or Worker's Compensation.
+
+Human Resources
+---------------
+ - Point of contact for Human Resources-related policy questions or issues.
+ - Work with Human Resources to ensure accurate documentation and tracking of FMLA issues.
+ - Work with Risk Management to ensure timely documentation of Worker's Comp related issues.
+" WHERE netid = "mstenber";
+
+
+UPDATE directory_employees SET photo = "http://library.nd.edu/about/departments/adminoff/images/MichelleSavoie.jpg" WHERE netid = "mstenber";
+
+
+
+UPDATE directory_employees SET about_text="
+As senior administrative assistant, Michele coordinates the activities of and provides primary support for the University Librarian (UL), which includes, in part, all clerical duties and collecting and posting departmental and committee annual reports. She also serves as a liaison between the Library Advisory Council members and University employees and coordinates meetings and events for the Advisory Council, the University Committee on Libraries and the Foik Award Committee.
+
+Administrative Support for University Librarian
+-----------------------------------------------
+
+ - Manage calendar, schedule meetings and appointments, and relay messages for the University Librarian.
+ - Take meeting minutes, prepare correspondence, obtain UL signatures, maintain files for Office of UL, make travel arrangements, and prepare expense reports for the University Librarian.
+ - Collect and post annual reports for all Library divisions and committees.
+ - Provide clerical support, coordinate events, maintain website, and serve as liaison for the University Committee on Libraries.
+
+Foik Award Committee
+--------------------
+ - Compile and manage nominations and committee activities of Foik Award Committee.
+ - Create a committee composed of 3 T&R faculty, 1 student, and the 3 most recent recipients of the Foik Award (invite members, provide instructions, give clerical support).
+ - Schedule meeting to choose award recipient, correspond with committee members, send instructions for selecting a recipient, and assist with writing the citation for the Provost.
+
+Library Advisory Council
+------------------------
+ - Maintain all correspondence files and contact information for members of the Library Advisory Council.
+ - Coordinate all activities, events, transportation needs, lodging, etc. for advisory council members.
+ - Work with the University Librarian and the Advisory Council Chair to create an agenda for Council weekends, and then provide all administrative support needed for the members.
+" WHERE netid = "mwolff";
+
+
+UPDATE directory_employees SET photo = "http://library.nd.edu/about/departments/adminoff/images/MicheleWolff.jpg" WHERE netid = "mwolff";
+
+
+
+UPDATE directory_employees SET about_text="
+In coordination with the Director and Senior Accounting Specialist, Lee is responsible for general and strategic financial analysis to aid in the development of the Library budget (approximately $27 million) with a focus on the materials acquisitions budget. He also serves as a liaison to Program Directors and Selectors to aid with budget questions and guidance.
+
+Budget
+------
+
+ - Assists with the preparation of the Library’s materials acquisition budget (approximately $11 million)
+ - Designs reports to aid in financial monitoring and planning
+ - Partners with several program directors to provide financial guidance for their budgets
+
+Analysis & Planning
+-------------------
+ - Assists the Director with strategic financial planning
+ - Identifies, develops, and recommends suggestions for process improvements
+ - Develops ad hoc reports on an as needed basis
+
+Acquisitions
+------------
+
+ - Manages the year-end process for closing the Acquisitions budget and rolling it over into the next fiscal year
+ - Manages payments of electronic products and use of ProCards for Acquisitions
+ - Works with Selectors to manage available funds
+" WHERE netid = "lpurdy1";
