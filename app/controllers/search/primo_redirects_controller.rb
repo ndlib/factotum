0ad961801e6edccd030f3aef8ab8831fc702be1d
Jaron Kennel <jaronkk@gmail.com>
2014-10-24 13:33:11 -0400

    Redirect primo searches using the deep link syntax

--- app/controllers/search/primo_redirects_controller.rb
+++ app/controllers/search/primo_redirects_controller.rb
@@ -1,20 +1,6 @@
 class Search::PrimoRedirectsController < ApplicationController
   def index
-    @redirect = redirect_class.new(params)
+    @redirect = Search::PrimoDeepLinkRedirect.new(params)
     redirect_to @redirect.url
-  end
-
-  private
-
-  def current_environment
-    Rails.env
-  end
-
-  def redirect_class
-    if current_environment == "production"
-      search_class = Search::PrimoRedirect
-    else
-      search_class = Search::PrimoDeepLinkRedirect
-    end
   end
 end
--- spec/controllers/search/primo_redirects_controller_spec.rb
+++ spec/controllers/search/primo_redirects_controller_spec.rb
@@ -2,22 +2,10 @@ require 'spec_helper'
 
 describe Search::PrimoRedirectsController do
   describe "#index" do
-    it "creates a redirect using the deep link" do
+    it "creates a redirect using the deep link syntax" do
       get :index, q: 'test', institution: 'NDU', tab: 'onesearch'
       expect(assigns(:redirect)).to be_a_kind_of Search::PrimoDeepLinkRedirect
       expect(response).to be_redirect
-    end
-
-    describe 'production' do
-      before do
-        subject.stub(:current_environment).and_return('production')
-      end
-
-      it "creates a redirect using the regular redirect" do
-      get :index, q: 'test', institution: 'NDU', tab: 'onesearch'
-      expect(assigns(:redirect)).to be_a_kind_of Search::PrimoRedirect
-      expect(response).to be_redirect
-    end
     end
   end
 end
