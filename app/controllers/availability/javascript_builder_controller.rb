b7a69b7381bf884fba61a356666fa8b0304fb12c
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-11-16 09:39:12 -0500

    fix the displayed url

--- app/controllers/availability/javascript_builder_controller.rb
+++ app/controllers/availability/javascript_builder_controller.rb
@@ -3,14 +3,20 @@ class Availability::JavascriptBuilderController < ApplicationController
 
   helper_method :current_url
 
-  def index  	
+  def index
     @selected_service_points = Availability::ServicePoint.where("code IN(?)", params[:selected_service_points]) || []
   end
 
   private
 
   def current_url
-    "#{request.protocol}#{request.host_with_port}"
+    if Rails.env.development?
+      "#{request.protocol}#{request.host_with_port}"
+    elsif Rails.env.production?
+      "https://factotum.library.nd.edu/"
+    else
+      "https://factotumpprd.library.nd.edu/"
+    end
   end
 
 
