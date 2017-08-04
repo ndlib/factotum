412eba0c2f4d596a5f7bbe8a1a19f1555f16a0cd
Robert Fox <rfox2@nd.edu>
2017-08-04 11:33:24 -0400

    Adding two book formats to request a purchase form

--- app/models/purchase_request.rb
+++ app/models/purchase_request.rb
@@ -84,7 +84,7 @@ class PurchaseRequest < ActiveRecord::Base
 
   NOTIFICATION_PREFERENCES = ["Email", "Phone", "Campus Mail"]
 
-  FORMATS = ["Book", "Journal", "Microform", "CD", "DVD", "Blu-ray", "Database", "Other"]
+  FORMATS = ["Book (ebook preferred)", "Book (print preferred)", "Journal", "Microform", "CD", "DVD", "Blu-ray", "Database", "Other"]
 
   validates_presence_of :requester_netid, :requester_name, :requester_email, :requester_notification_preference, :subject, :format, :title
   validates_inclusion_of :subject, in: SUBJECTS, allow_blank: true
