1b2875f4b3a06b99cd9f014c5eeed13ac3f5b4a8
Ryan Doughty <rdought1@nd.edu>
2018-01-22 08:53:11 -0500

    schema version update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214184409) do
+ActiveRecord::Schema.define(version: 20180119201734) do
 
   create_table "acquisition_exports", force: :cascade do |t|
     t.date     "start_date"
