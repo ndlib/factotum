4946de9ab230b73f88ef94dc8457422ccbee4253
Ryan Doughty <rdought1@nd.edu>
2018-02-14 11:26:22 -0500

    fixed typo in email address

--- app/mailers/acquisition_mailer.rb
+++ app/mailers/acquisition_mailer.rb
@@ -40,7 +40,7 @@ class AcquisitionMailer < ActionMailer::Base
     end
 
     def monographic_submission_recipient
-      ["monoacqorder@nd.edu","illiad@nd.edu@nd.edu"]
+      ["monoacqorder@nd.edu","illiad@nd.edu"]
     end
 
     def just_say_yes_submission_recipients
