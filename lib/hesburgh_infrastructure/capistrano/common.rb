b4914e4f45770074c2d412ab950b7a37ce304425
Ryan Doughty <rdought1@nd.edu>
2018-03-07 10:41:49 -0500

    undo del symlink

--- lib/hesburgh_infrastructure/capistrano/common.rb
+++ lib/hesburgh_infrastructure/capistrano/common.rb
@@ -21,7 +21,8 @@ Capistrano::Configuration.instance(:must_exist).load do
   _cset :application_symlinks, []
   _cset :default_symlinks, [
     '/log',
-    '/vendor/bundle'
+    '/vendor/bundle',
+    '/config/database.yml'
   ]
 
   _cset(:symlink_targets) { default_symlinks + application_symlinks }
