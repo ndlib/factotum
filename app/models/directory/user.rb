5cd7347f6dfa75377ffcbedd52a895ee99772236
Jon Hartzler <jon.hartzler@gmail.comcoke>
2019-06-04 09:10:02 -0400

    add new admin users

--- app/models/directory/user.rb
+++ app/models/directory/user.rb
@@ -9,7 +9,7 @@ class Directory::User
 
 
   def admin?
-    ['awetheri', 'rfox2', 'jhartzle', 'fboze', 'adixon4', 'msimons', 'mstenber', 'mwolff', 'dwolfe2', 'kkoski1', 'jgarces', 'tharrier', 'hbeachey', 'jbilins1', 'toleary2'].include?(self.username)
+    ['awetheri', 'rfox2', 'jhartzle', 'fboze', 'lajamie', 'mharden', 'adixon4', 'msimons', 'mstenber', 'mwolff', 'dwolfe2', 'kkoski1', 'jgarces', 'tharrier', 'jbilins1', 'toleary2'].include?(self.username)
   end
 
 end
