d96f13174c5cb43cd29e2e6fc6403752e9467363
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2019-12-19 09:16:26 -0500

    Removing usage of Airbrake
    With the addition of Sentry, we no longer need to send errors to
    Errbit. This relates to commits:
    
    * 6a1b27b0606f56e86d4ca04e4a98dd127525362a
    * dec0f815bea27186e59b664fefa353a62858ceae

--- config/initializers/airbrake.rb
+++
@@ -1,6 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key = "679f2a6383d89c807f5c5c7b8c0a39ff"
-  config.host    = "errbit.library.nd.edu"
-  config.port    = 443
-  config.secure  = config.port == 443
-end
