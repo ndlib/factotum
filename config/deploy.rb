3ba008e8f6b7a0ab9df67f3ecce08dc4cf35ca6b
Robert Fox <rfox2@nd.edu>
2021-01-20 09:37:24 -0500

    Bumping capistrano version for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.14.1"
 
 set :application, "factotum"
 set :repo_url, "git@github.com:ndlib/factotum.git"
