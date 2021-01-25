b6244d465e7d11a3497d30177608d786a63a3349
Robert Fox <rfox2@nd.edu>
2021-01-25 08:59:42 -0500

    Add CORS headers to directory api

--- app/controllers/directory/api_controller.rb
+++ app/controllers/directory/api_controller.rb
@@ -4,6 +4,15 @@ class Directory::ApiController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
   rescue_from ActionController::RoutingError, :with => :render_not_found
 
+  after_filter :cors_set_access_control_headers
+
+  # For all responses in this controller, return the CORS access control headers.
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
 
   def routing_error
     raise ActionController::RoutingError.new(params[:path])
